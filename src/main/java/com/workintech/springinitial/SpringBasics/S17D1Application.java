0e469ad232f029f65f7dc6c3bf9f11e7dda71514
goksu9898 <yesiladagoksu@gmail.com>
2024-04-08 00:23:20 +0300

    S17D1 projesi.

---
+++ src/main/java/com/workintech/springinitial/SpringBasics/S17D1Application.java
@@ -0,0 +1,14 @@
+package com.workintech.springinitial.SpringBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17D1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(S17D1Application.class, args);
+	}
+
+}
